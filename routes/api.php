4d7872afc2b71bb61881f3e887a2b001f4bcda3f
Santiago García <49987163+Sagalo04@users.noreply.github.com>
2022-07-18 19:59:06 -0500

    routes

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('heroes/{page}','App\Http\Controllers\HeroesController@getHeroes');
+
+Route::get('heroe/{id}','App\Http\Controllers\HeroesController@getSingleHeroe');
+
+Route::get('reactions','App\Http\Controllers\ReactionsController@getReactions');
+
+Route::post('addReaction','App\Http\Controllers\ReactionsController@insertReaction');
+
+Route::post('updateReaction/{heroe_id}','App\Http\Controllers\ReactionsController@updateReaction');
